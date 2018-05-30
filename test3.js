86c13c6bf43a2382aa436c4299294c8af6cbd602
Olga <89046555681@yandex.ru>
2018-05-30 19:27:35 +0300

    test3 modified

--- test3.js
+++ test3.js
@@ -1,7 +1,8 @@
 
 var a = Number(process.argv[2]);
 var b = Number(process.argv[3]);
-console.log(nod(a, b));
+//console.log(nod(a, b));
+process.stdout.write(String(nod(a, b)));
 
 
 function nod(n, m) {
