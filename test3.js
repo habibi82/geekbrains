a9d1fa01e1b06bb37da81caa75cc477566c59499
Olga <89046555681@yandex.ru>
2018-06-12 01:52:59 +0300

    test3 modified 12/06

--- test3.js
+++ test3.js
@@ -1,16 +1,17 @@
 
 var a = Number(process.argv[2]);
 var b = Number(process.argv[3]);
-//console.log(nod(a, b));
+
 process.stdout.write(String(nod(a, b)));
 
 
-function nod(n, m) {
-    if(m > 0) {
-        var k = n%m;
-        return nod(m, k);
+function nod(a, b) {
+    while (a !=0 && b !=0){
+        if(a > b) {
+            a = a%b;
+
+        }else b=b%a;
     }
-    else {
-        return Math.abs(n);
-    }
+    return a+b;
+
 }
